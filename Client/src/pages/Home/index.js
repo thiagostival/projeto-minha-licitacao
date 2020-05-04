4bb50fee1dc8de411000b9a689e82c7b202b2106
thiagostival <thiagofilipestival5@gmail.com>
2020-05-04 13:16:53 -0300

    update

--- Client/src/pages/Home/index.js
+++ Client/src/pages/Home/index.js
@@ -23,17 +23,19 @@ export default function Home(){
       <section id="conteudo">
         <div id="mensagem">
           <p id="slogan">As ofertas <br/>vem até você!</p>
-          <p id="titulo">O olist ajuda quem quer comprar a encontrar quem quer vender </p>
+          <p id="titulo">Ajudamos quem quer comprar a encontrar quem quer vender </p>
           <div id="vantagens">
             <div id="text">
               <img className="check" src={checkImg} alt="Check" /> 
-              Especifique todas as características do que está procurando e espere pelas propostas dos mais de 500 mil lojistas cadastrados na Olist.	
+              Especifique todas as características do que está procurando e espere 
+              pelas propostas dos mais de 500 mil lojistas cadastrados na plataforma.	
             </div>
             <div id="text">
               <img className="check" src={checkImg} alt="Check" />
-              Ao invés de ficar buscando ofertas na internet nossos vendedores levam a proposta até você de forma personalizada que atenda a sua demanda.
+              Ao invés de ficar buscando ofertas na internet nossos vendedores levam 
+              a proposta até você de forma personalizada que atenda a sua demanda.
             </div>
-            <div><button className="button">Quero comprar na Olist</button></div>
+            <div><button className="button">Quero comprar</button></div>
           </div>
         </div>
         <img className="home-bg" src={contractImg} alt="Contract" />
